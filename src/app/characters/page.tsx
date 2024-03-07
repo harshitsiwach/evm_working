fb0d6902ef64887695eb9405c24bca511601871b
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-03-08 00:24:16 +0530

    hdasd

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -3,43 +3,54 @@ import Image from "next/image"
 function page() {
   return (
     <div className="h-screen gap-5 w-screen flex items-center justify-center">
-      
+      <div className=" border relative rounded-lg border-gray-800 overflow-hidden ">
         <Image
-         className="hover:blur-sm ease-linear cursor-pointer border rounded-lg border-gray-800 "
+         className="hover:blur-sm  hover:scale-125 ease-linear cursor-pointer transition-transform duration-300 "
         src="/Characters/C1.png"
         alt="alt"
         height={280}
         width={280}
         />
-        
+        <div className="absolute top-0 left-0 w-full h-full flex items-end justify-center bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-linear">
+        <p className="text-white text-xl">Your Text Here</p>
+       </div>
+        </div>
+        <div className=" border rounded-lg border-gray-800 overflow-hidden ">
         <Image
-        className="hover:blur-sm ease-linear cursor-pointer border rounded-lg border-gray-800 "
+        className="hover:blur-sm  hover:scale-125 ease-linear cursor-pointer transition-transform duration-300  "
         src="/Characters/C5.png"
         alt="alt"
         height={280}
         width={280}
         />
+        </div>
+        <div className=" border rounded-lg border-gray-800 overflow-hidden ">
         <Image
-        className="hover:blur-sm ease-linear cursor-pointer border rounded-lg border-gray-800 "
+        className="hover:blur-sm  hover:scale-125 ease-linear cursor-pointer transition-transform duration-300  "
         src="/Characters/C3.png"
         alt="alt"
         height={280}
         width={280}
         />
+        </div>
+        <div className=" border rounded-lg border-gray-800 overflow-hidden ">
         <Image
-        className="hover:blur-sm ease-linear cursor-pointer border rounded-lg border-gray-800 "
+        className="hover:blur-sm  hover:scale-125 ease-linear cursor-pointer transition-transform duration-300  "
         src="/Characters/C4.png"
         alt="alt"
         height={280}
         width={280}
         />
+      </div>
+      <div className=" border rounded-lg border-gray-800 overflow-hidden ">
         <Image
-        className="hover:blur-sm ease-linear cursor-pointer  border rounded-lg border-gray-800 "
+        className="hover:blur-sm  hover:scale-125 ease-linear cursor-pointer transition-transform duration-300  "
         src="/Characters/C2.png"
         alt="alt"
         height={280}
         width={280}
         />
+        </div>
     </div>
   )
 }
