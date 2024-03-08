64851c2abe08c60d049f347834e4b433cf153433
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-03-09 04:07:19 +0530

    changed env

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,38 +1,9 @@
-"use client"
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from 'react'
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-
-
-
-// import required modules
-import { Navigation } from 'swiper/modules';
-
-export default function App() {
+function page() {
   return (
-    <>
-    
-    <Swiper
-      spaceBetween={30} // Space between slides
-      centeredSlides={true} // Active slide will be centered
-      slidesPerView={'auto'} // Adjust 'auto' or specific number as per design
-      loop={true} // Enables loop mode
-      navigation // Enables navigation buttons
-      pagination={{ clickable: true }} // Enables and configures pagination
-      // You can adjust "centeredSlidesBounds" and "slidesOffsetBefore/after" to control the visibility of previous/next images
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide><img src="/Game UI/1.png" alt="Slide 1"/></SwiperSlide>
-      <SwiperSlide><img src="/Game UI/1.png" alt="Slide 2"/></SwiperSlide>
-      <SwiperSlide><img src="/Game UI/1.png" alt="Slide 3"/></SwiperSlide>
-      {/* ...more slides */}
-    </Swiper>
-      
-    </>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
