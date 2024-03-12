0f5cad48ebc567f37852bbaf9a31a6e994af30a4
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-03-13 00:01:49 +0530

    changed footer and logos

--- src/app/socials/page.tsx
+++ src/app/socials/page.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/Utils/cn";
 import React from "react";
 import { BentoGrid, BentoGridItem } from "@/Components/ui/bento-grid";
+import { Tweet } from "react-tweet";
+
 import {
   IconArrowWaveRightUp,
   IconBoxAlignRightFilled,
@@ -12,20 +14,30 @@ import {
 } from "@tabler/icons-react";
 
 function page() {
+  interface InstagramEmbedProps {
+    url: string,
+    clientAccessToken: string,
+    maxWidth: number,
+    hideCaption: boolean,
+    containerTagName: string,
+    protocol: string,
+  }
   return (
-    <div className="mt-[80px] mb-[80px]">
-       <BentoGrid className="max-w-4xl mx-auto">
-      {items.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          icon={item.icon}
-          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
-        />
-      ))}
+    <div className="mt-[80px] ">
+       <BentoGrid className="w-auto max-w-4xl mx-auto h-auto flex flex-row gap-10">
+       <div>
+       <Tweet  id="1763074266294337614" />
+       </div>
+       <iframe src="https://discord.com/widget?id=1088556262511349801&theme=dark" width="350" height="500" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
     </BentoGrid> 
+    <BentoGrid className="w-[300px] max-w-4xl mx-auto h-auto">
+       
+   
+       
+    </BentoGrid>
+       
+    
+    
    
     </div>
     
@@ -34,50 +46,6 @@ function page() {
 const Skeleton = () => (
     <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
   );
-  const items = [
-    {
-      title: "The Dawn of Innovation",
-      description: "Explore the birth of groundbreaking ideas and inventions.",
-      header: <Skeleton />,
-      icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Digital Revolution",
-      description: "Dive into the transformative power of technology.",
-      header: <Skeleton />,
-      icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Art of Design",
-      description: "Discover the beauty of thoughtful and functional design.",
-      header: <Skeleton />,
-      icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Power of Communication",
-      description:
-        "Understand the impact of effective communication in our lives.",
-      header: <Skeleton />,
-      icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Pursuit of Knowledge",
-      description: "Join the quest for understanding and enlightenment.",
-      header: <Skeleton />,
-      icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Joy of Creation",
-      description: "Experience the thrill of bringing ideas to life.",
-      header: <Skeleton />,
-      icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Spirit of Adventure",
-      description: "Embark on exciting journeys and thrilling discoveries.",
-      header: <Skeleton />,
-      icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
-    },
-  ];
+ 
 
 export default page
