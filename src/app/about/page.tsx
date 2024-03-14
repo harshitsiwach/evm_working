ab868c33d0c11595bf1b7a7bfeef562996080c04
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-03-15 01:51:59 +0530

    changed about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,115 +1,66 @@
-"use client"
+
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
-import './custom-swiper.css';
+
 import Image from 'next/image';
 import Marquee from "react-fast-marquee";
 function page() {
   
   return (
  
-    <div className=' h-screen flex flex-col items-center justify-center '>
-        <div className='w-full sm:w-5/6 h-full flex justify-center items center '>
-          
-     <Swiper navigation={true} modules={[Navigation]} className="mySwiper  ">
-     
-        <SwiperSlide >
-            <div className=' h-full gap-10  w-full flex flex-col items-start justify-center  text-[#F52239] p-4 'style={{fontFamily : "PressStart2P"}}>
-                <div className='text-3xl ml-10 mr-5 sm:text-5xl'>
-                
-                What Is EVM Warfare
-                </div>
-                <div className='text-md text-white ml-10 mr-5  ' style={{fontFamily : "PlusJakarta"}}>
-               
-                EVM Warfare is a visceral third-person shooter built on Unreal Engine 5. It seamlessly integrates with all Ethereum Virtual Machine (EVM) compatible blockchains, allowing players to experience unparalleled interoperability and strategic depth. Immerse yourself in the EVM Wars, a high-stakes arena where the fight for survival transcends the boundaries of the digital world.
-            </div>
-            </div>
-           
-        </SwiperSlide>
-        <SwiperSlide >
-        <div className=' h-full gap-10  w-full flex flex-col items-start justify-center  text-[#F52239] p-4 'style={{fontFamily : "PressStart2P"}}>
-                <div className='text-3xl ml-10 mr-5 sm:text-5xl'>
-                
-                Features
-                </div>
-                <div className='text-md text-white ml-10 mr-5  ' style={{fontFamily : "PlusJakarta"}}>
-               
-                Fight, Earn, Dominate. <br />
-                <br />
+    <div className=' h-auto sm:h-screen mt-[80px] mb-[160px] overflow-auto gap-5 flex flex-col items-center justify-center '>
+         <div className=" text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-[#F52239] font-PressStart2p"style={{fontFamily : "PressStart2P"}}>
+            ABOUT
+          </div>
+          <div className="text-[#F52239] w-5/6 sm:w-[700px]  flex flex-col justify-start"style={{fontFamily : "PressStart2P"}}>
+          What is EVM Warfare? <br />
+            <div className="text-white  text-sm"style={{fontFamily : "PlusJakarta"}}>
+            EVM Warfare is a visceral third-person shooter built on Unreal Engine 5. It seamlessly integrates with all Ethereum Virtual Machine (EVM) compatible blockchains, allowing players to experience unparalleled interoperability and strategic depth. Immerse yourself in the EVM Wars, a high-stakes arena where the fight for survival transcends the boundaries of the digital world.            </div>
+          </div>
+          <div className=" text-[#F52239] w-5/6 sm:w-[700px] flex flex-col justify-start"style={{fontFamily : "PressStart2P"}}>
+          Features <br />
+            <div className="text-white"style={{fontFamily : "PlusJakarta"}}>
+            Fight, Earn, Dominate.
 
-                Thrilling third-person shooter powered by Unreal Engine 5. Deposit via EVM blockchain. Dominate deathmatches, claim instant rewards based on performance. Enter the EVM: Desperation Protocol. Skill meets blockchain glory.
-            </div>
-            </div>
-           
-        </SwiperSlide>
-        <SwiperSlide >
-        <div className=' h-full gap-10  w-full flex flex-col items-start justify-center  text-[#F52239] p-4 'style={{fontFamily : "PressStart2P"}}>
-                <div className='text-3xl ml-10 mr-5 sm:text-5xl'>
-                
-                Features
-                </div>
-                <div className='text-md text-white ml-10 mr-5  ' style={{fontFamily : "PlusJakarta"}}>
-                Effortlessly manage your digital assets with our integrated, universal EVM wallet <br />
-                <br />
-                Dualsense /Xbox Controller Support <br />
-                <br />
-                Deposit and withdraw funds seamlessly across all EVM-compatible blockchains. Create a new wallet or import your existing one with a single click.Send and receive funds to anyone within the game. Check Live GAS, Balance and Block info.            </div>
-            </div>
-           
-        </SwiperSlide>
-        <SwiperSlide >
-        <div className=' h-full gap-10  w-full flex flex-col items-start justify-center  text-[#F52239] p-4 'style={{fontFamily : "PressStart2P"}}>
-                <div className='text-3xl ml-10 mr-5 sm:text-5xl'>
-                Upcoming
-                </div>
-                <div className='text-md text-white ml-10 mr-5  ' style={{fontFamily : "PlusJakarta"}}>
-                Gasless Transactions <br />
-                <br />
-                Two-Factor Authentication (2FA) <br />
-                <br />
-                Cross-Chain ERC-20 Tokens <br />  
-                <br />
-                In-Game Cross-Chain Decentralized Exchange (DEX) and NFT Marketplace            </div>
-            </div>
-           
-        </SwiperSlide>
-        <SwiperSlide >
-        <div className=' h-full gap-10  w-full flex flex-col items-start justify-center  text-[#F52239] sm:p-4 p-2 'style={{fontFamily : "PressStart2P"}}>
-                <div className='text-md sm:ml-10 ml-0 sm:mr-5 mr-0 sm:text-5xl'>
-                Building Community - [gamers/developers]
+            Thrilling third-person shooter powered by Unreal Engine 5. Deposit via EVM blockchain. Dominate deathmatches, claim instant rewards based on performance. Enter the EVM: Desperation Protocol. Skill meets blockchain glory.              </div>
+           </div>
+           <div className=" text-[#F52239] w-5/6 sm:w-[700px] flex flex-col justify-start"style={{fontFamily : "PressStart2P"}}>
+            Features 
+            <div className="text-white"style={{fontFamily : "PlusJakarta"}}>
+            Effortlessly manage your digital assets with our integrated, universal EVM wallet 
 
-                </div>
-                <div className='text-xl ml-10 mr-5 sm:text-3xl'>
-                Cultivating Gamers:
-                </div>
-                <div className='text-md text-white ml-10 mr-5  ' style={{fontFamily : "PlusJakarta"}}>
-                Elevating the competitive spirit, we will be introducing prizes for leaderboard champions on both testnet and mainnet. By recognizing top performers, we aim to ignite excitement and camaraderie among players, driving engagement and excellence within our gaming ecosystem.           </div>
-            </div>
-           
-        </SwiperSlide>
-        <SwiperSlide >
-        <div className=' h-full gap-10  w-full flex flex-col items-start justify-center  text-[#F52239] sm:p-4 p-2'style={{fontFamily : "PressStart2P"}}>
-                <div className='text-md sm:ml-10 ml-0 sm:mr-5 mr-0 sm:text-5xl'>
-                Building Community - [gamers/developers]
-                </div>
-                <div className='text-xl ml-10 mr-5 sm:text-3xl'>
-                Farming Developers:
-                </div>
-                <div className='text-md text-white ml-10 mr-5  ' style={{fontFamily : "PlusJakarta"}}>
-                Embracing transparency and collaboration, we will be open-sourcing our codebase and providing comprehensive educational resources. This initiative invites aspiring developers to learn, innovate, and contribute to our project, fostering a dynamic community of creators dedicated to pushing the boundaries of blockchain gaming technology.          </div>
-            </div>
-           
-        </SwiperSlide>
-       
-     
-      </Swiper>
-      </div>
+            Dualsense /Xbox Controller Support 
+
+            Deposit and withdraw funds seamlessly across all EVM-compatible blockchains. Create a new wallet or import your existing one with a single click.Send and receive funds to anyone within the game. Check Live GAS, Balance and Block info.            </div>
+          </div>
+          <div className=" text-[#F52239] w-5/6 sm:w-[700px] flex flex-col justify-start"style={{fontFamily : "PressStart2P"}}>
+          Upcoming 
+            <div className="text-white"style={{fontFamily : "PlusJakarta"}}>
+            Gasless Transactions 
+
+            Two-Factor Authentication (2FA)
+
+            Cross-Chain ERC-20 Tokens
+
+            In-Game Cross-Chain Decentralized Exchange (DEX) and NFT Marketplace            </div>
+          </div>
+          <div className=" text-[#F52239] w-5/6 sm:w-[700px] flex flex-col justify-start"style={{fontFamily : "PressStart2P"}}>
+          Building Community - [gamers/developers] 
+            <div className="text-white"style={{fontFamily : "PlusJakarta"}}>
+            Cultivating Gamers:
+
+          Elevating the competitive spirit, we will be introducing prizes for leaderboard champions on both testnet and mainnet. By recognizing top performers, we aim to ignite excitement and camaraderie among players, driving engagement and excellence within our gaming ecosystem.               </div>
+          </div>
+
+          {/* <div className=" text-[#F52239] w-5/6 sm:w-[700px] flex flex-col justify-start"style={{fontFamily : "PressStart2P"}}>
+          Building Community - [gamers/developers]
+            <div className="text-white"style={{fontFamily : "PlusJakarta"}}>
+            Farming Developers:
+
+Embracing transparency and collaboration, we will be open-sourcing our codebase and providing comprehensive educational resources. This initiative invites aspiring developers to learn, innovate, and contribute to our project, fostering a dynamic community of creators dedicated to pushing the boundaries of blockchain gaming technology.               </div>
+          </div> */}
       
-      <div className='w-3/6 h-[200px]'>
-      <Marquee className=' corner-fade mb-[80px]  'gradient={true} gradientColor='black'>
+      <div className='fixed bg-black bottom-0 w-3/6 h-[150px]'>
+      <Marquee className=' corner-fade bg-black  'gradient={true} gradientColor='black'>
        
       <svg width="100" height="50" viewBox="0 0 1503 1504" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect x="287" y="258" width="928" height="844" fill="white"/>
