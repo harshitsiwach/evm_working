e155996721d335a2fe2f1d60afa2d9e1857aaf26
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-04-07 05:22:38 +0530

    changed team names

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -3,7 +3,21 @@ import Image from "next/image"
 
 function page() {
   return (
-    <div className='flex flex-col sm:mt-0 mt-[80px] items-center justify-center sm:h-screen overflow-hidden  gap-5 sm:flex sm:flex-row sm:justify-center sm:items-center'>
+    <div className='dark:bg-black flex flex-col sm:mt-0 mt-[80px] items-center justify-center sm:h-screen overflow-hidden  gap-5 sm:flex sm:flex-row sm:justify-center sm:items-center'>
+         <div className='h-[50px] hover:h-[500px] sm:h-[500px] sm:w-[100px] sm:hover:w-[400px] ease-in-out duration-300 cursor-pointer bg-red-800 '>
+            <Image
+            className="object-cover h-full"
+            src='/Team/7989556.jpg'
+            alt="star"
+            height={400}
+            width={400}
+            />
+            <div className="opacity-0 hover:opacity-100 ml-5  ease-in-out duration-300  h-full  relative  overflow-hidden bottom-[490px] ">
+            0xUgly <br />
+            Founder
+            </div>
+        </div>
+        
         <div className='h-[50px] hover:h-[500px] sm:h-[500px] sm:w-[100px] sm:hover:w-[400px] ease-in-out duration-300 cursor-pointer bg-red-800 '>
           
             <Image
@@ -14,7 +28,7 @@ function page() {
             width={400}
           
             />
-            <div className="opacity-0 hover:opacity-100 ml-5  ease-in-out duration-300  h-full  relative  overflow-hidden bottom-[450px] ">
+            <div className="opacity-0 hover:opacity-100 ml-5  ease-in-out duration-300  h-full  relative  overflow-hidden bottom-[490px] ">
               Artist <br />
               HAZARD
             </div>
@@ -29,6 +43,10 @@ function page() {
             height={400}
             width={400}
             />
+            <div className="opacity-0 hover:opacity-100 ml-5  ease-in-out duration-300  h-full  relative  overflow-hidden bottom-[490px] ">
+            MilitantNoob <br />
+            Sr. Game Dev.
+            </div>
         </div>
 
         <div className='h-[50px] hover:h-[500px] sm:h-[500px] sm:w-[100px] sm:hover:w-[400px] ease-in-out duration-300 cursor-pointer bg-red-800 '>
@@ -39,6 +57,10 @@ function page() {
             height={400}
             width={400}
             />
+            <div className="opacity-0 hover:opacity-100 ml-5  ease-in-out duration-300  h-full  relative  overflow-hidden bottom-[490px] ">
+            mrSniper <br />
+            Jr. Game Dev.
+            </div>
         </div>
 
         <div className='h-[50px] hover:h-[500px] sm:h-[500px] sm:w-[100px] sm:hover:w-[400px] ease-in-out duration-300 cursor-pointer bg-red-800 '>
@@ -49,7 +71,10 @@ function page() {
             height={400}
             width={400}
             />
-
+            <div className="opacity-0 hover:opacity-100 ml-5  ease-in-out duration-300  h-full  relative  overflow-hidden bottom-[490px] ">
+            0xDinu <br />
+            Web Dev.
+            </div>
         </div>
 
         <div className='h-[50px] hover:h-[500px] sm:h-[500px] sm:w-[100px] sm:hover:w-[400px] ease-in-out duration-300 cursor-pointer bg-red-800 '>
@@ -60,6 +85,10 @@ function page() {
             height={400}
             width={400}
             />
+            <div className="opacity-0 hover:opacity-100 ml-5  ease-in-out duration-300  h-full  relative  overflow-hidden bottom-[490px] ">
+            David <br />
+            Smart Contract auditor
+            </div>
         </div>
 
         <div className='h-[50px] hover:h-[500px] sm:h-[500px] sm:w-[100px] sm:hover:w-[400px] ease-in-out duration-300 cursor-pointer bg-red-800 '>
@@ -70,17 +99,13 @@ function page() {
             height={400}
             width={400}
             />
+            <div className="opacity-0 hover:opacity-100 ml-5  ease-in-out duration-300  h-full  relative  overflow-hidden bottom-[490px] ">
+            Sinister <br />
+            Data Scientist
+            </div>
         </div>
 
-        <div className='h-[50px] hover:h-[500px] sm:h-[500px] sm:w-[100px] sm:hover:w-[400px] ease-in-out duration-300 cursor-pointer bg-red-800 '>
-            <Image
-            className="object-cover h-full"
-            src='/Team/7989556.jpg'
-            alt="star"
-            height={400}
-            width={400}
-            />
-        </div>
+       
 
         <div className='h-[50px] hover:h-[500px] sm:h-[500px] sm:w-[100px] sm:hover:w-[400px] ease-in-out duration-300 cursor-pointer bg-red-800 '>
             <Image
@@ -90,6 +115,10 @@ function page() {
             height={400}
             width={400}
             />
+            <div className="opacity-0 hover:opacity-100 ml-5  ease-in-out duration-300  h-full  relative  overflow-hidden bottom-[490px] ">
+            RiseBoi <br />
+            Collabs Manager
+            </div>
         </div>
     </div>
   )
