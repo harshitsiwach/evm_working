220ea1d957802872c7127a6bb3f3326996694fb3
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-04-14 04:52:41 +0530

    fixed error

--- src/app/socials/page.tsx
+++ src/app/socials/page.tsx
@@ -1,6 +1,6 @@
     "use client";
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Tweet } from 'react-tweet'
 import { twMerge } from "tailwind-merge";
 import { TracingBeam } from "@/Components/ui/tracing-beam";
@@ -9,6 +9,12 @@ import { HeroHighlight, Highlight } from "@/Components/ui/hero-highlight";
 import Image from 'next/image';
 import Marquee from "react-fast-marquee";
 export default function page() {
+      const [isClient, setIsClient] = useState(false);
+
+      useEffect(() => {
+        // Component did mount in client
+        setIsClient(true);
+      }, []);
   return (
     <>
     <HeroHighlight >
@@ -31,7 +37,7 @@ export default function page() {
         </div>
         </div>
         <div className=''>
-     <iframe className='mt-3' src="https://discord.com/widget?id=1088556262511349801&theme=dark" width="400" height="480" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        {isClient && ( <iframe className='mt-3' src="https://discord.com/widget?id=1088556262511349801&theme=dark" width="400" height="480" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>)}
         </div>
      </div>
 
@@ -49,16 +55,24 @@ export default function page() {
         </div>
         
     
-    <div className=' m-3'>
-    <iframe frameBorder="0" src="https://itch.io/embed/2570965?bg_color=222222&amp;fg_color=eeeeee&amp;link_color=be2424&amp;border_color=363636" width="552" height="167"><a href="https://evmwarfare.itch.io/evmwarfare">EVM Warfare by evmwarfare</a></iframe>
-
-    </div>
+        <div className='m-3'>
+        {isClient && ( <iframe 
+        frameBorder="0" 
+        src="https://itch.io/embed/2570965?bg_color=222222&fg_color=eeeeee&link_color=be2424&border_color=363636" 
+        width="552" 
+        height="167" 
+        style={{border: 'none'}}  // Ensure border style is applied correctly
+    >
+        <a href="https://evmwarfare.itch.io/evmwarfare">EVM Warfare by evmwarfare</a>
+    </iframe>
+        )}
+</div>
    
 
      
     </div>
     <div className='mt-5  w-full flex flex-row items-center justify-center '>
-    <iframe width="660" height="315" src="https://www.youtube.com/embed/IRSmYXty8z0?si=A9Y7V8qNBF9nGDPB" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    {isClient && (<iframe width="660" height="315" src="https://www.youtube.com/embed/IRSmYXty8z0?si=A9Y7V8qNBF9nGDPB" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>)}
     </div>
     <div className=' mt-[100px]  w-full flex flex-row items-center justify-center sm:w-[1000px] h-[150px]'>
       <div className=' w-screen sm:w-4/6 h-[150px]'>
