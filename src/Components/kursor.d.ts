d2ade4f5568282a50963c735c226d13c1026d064
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-05-24 23:29:22 +0530

    changed cursor

---
+++ src/Components/kursor.d.ts
@@ -0,0 +1,5 @@
+declare module 'kursor' {
+    export default class Kursor {
+      constructor(options: { type: number; color?: string; removeDefaultCursor?: boolean; });
+    }
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-
+"use client"
+import Kursor from "kursor";
+import "kursor/dist/kursor.css";
+import React, { useEffect } from 'react';
 import Gradient from "../Components/gradient";
 
 
 export default function Home() {
+
+    new Kursor({
+      type: 3,
+      color: '#F52239',
+      removeDefaultCursor: true
+    });
+  
   return (
+  
    <Gradient/>
+
     
   );
 }
